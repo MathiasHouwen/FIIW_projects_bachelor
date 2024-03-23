3f1ba05d2595a6d4cfcd3bf170bd8e25b156cbbd
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-03-23 23:45:11 +0100

    En begin gemaakt

--- Practicum 2/practicum-2-team-12-ebbe-mathias/Main.c
+++ Practicum 2/practicum-2-team-12-ebbe-mathias/Main.c
@@ -1,6 +1,28 @@
+#define hoeveelheidStock 5 // VERANDER NAAR 20
 
 typedef struct {
 	int productID;
 	int productCount;
 } product;
 
+product stock[hoeveelheidStock];
+
+int main() {
+	return 0;
+}
+
+void initialiseerStock() {
+	product iPhone = { 0, 50 }; //EBBE PAS DIT AUB AAN DAT DAT IN EEN FORLOOP WORDT GEDIFEND
+	product lenovoLaptop = { 1, 50 }; // IDK HOW
+	product samsungPhone = { 2, 50 };
+	product windowsPhone = { 3, 50 };
+	product hpLaptop = { 4, 50 };
+}
+
+void koop(int index, int aantal) {
+	//
+}
+
+void produceer(int index, int aantal) {
+	//
+}

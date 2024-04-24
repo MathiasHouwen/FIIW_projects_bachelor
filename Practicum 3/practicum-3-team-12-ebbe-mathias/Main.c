d3d60812936b6d593163417b783c51bedbc58014
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-04-24 15:48:42 +0200

    Added insertMovieScript en testcode :)

--- Practicum 3/practicum-3-team-12-ebbe-mathias/Main.c
+++ Practicum 3/practicum-3-team-12-ebbe-mathias/Main.c
@@ -31,13 +31,16 @@ struct MovieNode {
 MovieData* newMovie(const char* name, int year);
 ActorData* newActor(const char* name);
 int compareMovies(MovieData* movie1, MovieData* movie2);
+void insertMovie(MovieNode** head, MovieNode* newNode);
+MovieNode* newMovieNode(MovieData* movie);
 
 void movieTestCode();
 void acteurTestCode();
 void compareMoviesTestCode();
+void insertMovieTestCode();
 
 int main() {
-    compareMoviesTestCode();
+    insertMovieTestCode();
     return 0;
 }
 
@@ -87,6 +90,50 @@ int compareMovies(MovieData* movie1, MovieData* movie2) {
     return movie1->year - movie2->year;
 }
 
+int compareActors(ActorData* actor1, ActorData* actor2) {
+    return strcmp(actor1->name, actor2->name);
+}
+
+// Functie om een nieuwe filmstructuur te maken
+MovieNode* newMovieNode(MovieData* movie) {
+    // Geheugen toewijzen
+    MovieNode* new_node = (MovieNode*)malloc(sizeof(MovieNode));
+    if (new_node == NULL) {
+        printf("Geheugen kon niet worden toegewezen voor de nieuwe node.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->movie = movie;
+    new_node->next = NULL;
+
+    return new_node;
+}
+
+void insertMovie(MovieNode** head, MovieNode* newNode) {
+    MovieNode* current = *head;
+    MovieNode* prev = NULL;
+
+    // Ga door de lijst tot bij de plaats waar die moet geinsert worden
+    while (current != NULL && compareMovies(newNode->movie, current->movie) > 0) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (prev == NULL) {
+        // Head van linkedlist TODO: THIS IS SOMEHOW BAD FIX MATHIAS
+        newNode->next = *head;
+        *head = newNode;
+    } else if (current == NULL) {
+        // Tail van de linkedlist
+        prev->next = newNode;
+        newNode->next = NULL;
+    } else {
+        // Middel van de linkedlist
+        prev->next = newNode;
+        newNode->next = current;
+    }
+}
+
 /* =============================[TEST CODE]===========================================*/
 void movieTestCode() {
     MovieData* movie1 = newMovie("Inception", 2010);
@@ -113,17 +160,40 @@ void acteurTestCode() {
 void compareMoviesTestCode() {
     // Maak een array van films
     MovieData movies[] = {
-        {"b", 2010},
-        {"a", 2008},
-        {"b", 1994}
+        {"Inception", 2010},
+        {"The Shawshank Redemption", 1994},
+        {"Pulp Fiction", 1994},
+        {"The Godfather", 1972},
+        {"Forrest Gump", 1994}
     };
 
     // Sorteer de array van films
-    qsort(movies, sizeof(movies) / sizeof(movies[0]), sizeof(struct MovieData), compareMovies);
+    qsort(movies, sizeof(movies) / sizeof(movies[0]), sizeof(struct MovieData),
+          (_CoreCrtNonSecureSearchSortCompareFunction) compareMovies);
 
     // Geef de gesorteerde films weer
     printf("Gesorteerde films:\n");
     for (int i = 0; i < sizeof(movies) / sizeof(movies[0]); i++) {
         printf("%s (%d)\n", movies[i].name, movies[i].year);
     }
+}
+
+void insertMovieTestCode() {
+    MovieData* movie1 = newMovie("Inception", 2010);
+    MovieData* movie2 = newMovie("The Shawshank Redemption", 1994);
+    MovieData* movie3 = newMovie("Pulp Fiction", 1994);
+    MovieData* movie4 = newMovie("The Godfather", 1972);
+    MovieData* movie5 = newMovie("Forrest Gump", 1994);
+
+    MovieNode* movieNode1 = newMovieNode(movie1);
+    MovieNode* movieNode2 = newMovieNode(movie2);
+    MovieNode* movieNode3 = newMovieNode(movie3);
+    MovieNode* movieNode4 = newMovieNode(movie4);
+    MovieNode* movieNode5 = newMovieNode(movie5);
+
+    MovieNode** head =&movieNode1;
+    insertMovie(head, movieNode2);
+    insertMovie(head, movieNode3);
+    insertMovie(head, movieNode4);
+    insertMovie(head, movieNode5);
 }

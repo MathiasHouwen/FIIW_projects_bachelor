fc4766f4905a13e406047bbadbd75f7b75b1ab74
RobinLambregts <robin.lambregts@student.uhasselt.be>
2024-10-04 16:43:00 +0200

    Board class

--- Chaturaji/src/model/Board.cpp
+++ Chaturaji/src/model/Board.cpp
@@ -1,3 +1,26 @@
-
-
 #include "Board.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+bool Board::getCell(int row, int col) {
+    return board[row][col];
+}
+
+void Board::setCell(int row, int col, bool value) {
+    board[row][col] = value;
+}
+
+void Board::move(int toRow, int toCol, int fromRow, int fromCol) {
+    if(fromRow + toRow <= width * 2
+    && fromCol + toCol <= height * 2
+    && toCol >= 0
+    && toRow >= 0) {
+         setCell(fromRow, fromCol, false);
+         setCell(toRow, toCol, true);
+    }
+    else{cout << "invalid cell at " << toRow << ", " << toCol << endl;}
+}
+
+
--- Chaturaji/src/model/Board.h
+++ Chaturaji/src/model/Board.h
@@ -5,6 +5,15 @@
 
 
 class Board {
+    private:
+        static const int height = 8;
+        static const int width = 8;
+        bool board[width][height];
+
+    public:
+        void setCell(int row, int col, bool value);
+        bool getCell(int row, int col);
+        void move(int toRow, int toCol, int fromRow, int fromCol);
 
 };
 

7c9733849621614c82df5da48af6ff7b9cc8d4f3
RobinLambregts <robin.lambregts@student.uhasselt.be>
2024-10-16 16:21:09 +0200

    Agenda classes but datastructure has to be decided

--- agenda/charedClasses/Agenda.cpp
+++ agenda/charedClasses/Agenda.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Agenda.h"
+
+#include <utility>
+
+Agenda::Agenda(std::string name)
+            : name(std::move(name)) {
+}
+
+std::vector<Events> Agenda::getEvents() {
+    return events;
+}
+
+void Agenda::addEvent(Events event) {
+    // TODO: hangt van datastructuur af
+}
+
+void Agenda::removeEvent(Events event) {
+    // TODO: hangt van datastructuur af
+}
+
+std::string Agenda::getName() {
+    return name;
+}
+
+
--- agenda/charedClasses/Agenda.h
+++ agenda/charedClasses/Agenda.h
@@ -5,10 +5,22 @@
 #ifndef AGENDA_H
 #define AGENDA_H
 
+#include <string>
+#include <vector>
 
+#include "Events.h"
 
 class Agenda {
-
+    private:
+        std::string name;
+        std::vector<Events> events; // TODO: andere datastructuur gebruiken
+    public:
+        explicit Agenda(std::string name);
+        ~Agenda() = default;
+        std::vector<Events> getEvents(); // TODO: hangt van datastructuur af
+        void addEvent(Events event);
+        void removeEvent(Events event);
+        std::string getName();
 };
 
 

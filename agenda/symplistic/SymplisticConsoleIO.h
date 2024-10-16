0fd59b4b4fc619709af71b37dd90eaf1a5e11478
RobinLambregts <robin.lambregts@student.uhasselt.be>
2024-10-16 14:40:55 +0200

    ConsoleIO class added

---
+++ agenda/symplistic/SymplisticConsoleIO.h
@@ -0,0 +1,16 @@
+//
+// Created by robin on 16/10/2024.
+//
+
+#ifndef SYMPLISTICCONSOLEIO_H
+#define SYMPLISTICCONSOLEIO_H
+
+
+
+class SymplisticConsoleIO {
+
+};
+
+
+
+#endif //SYMPLISTICCONSOLEIO_H

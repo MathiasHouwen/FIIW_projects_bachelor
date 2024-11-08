2c929be5cdedc927ffeffe8dfc055f83f9332501
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-11-08 15:40:32 +0100

    addes asset folders

--- Chaturaji/src/view/PieceView.cpp
+++ Chaturaji/src/view/PieceView.cpp
@@ -10,11 +10,10 @@ PieceView::PieceView(QWidget *parent)
         : QWidget(parent) {
     QVBoxLayout *layout = new QVBoxLayout;
 
-    // QLabel to display the image
     QLabel *imageLabel = new QLabel(this);
 
     // Load the image into a QPixmap
-    QPixmap pixmap("C:/Users/houwe/Downloads/Chess_prg45.svg"); // Replace with your image file path
+    QPixmap pixmap("Chaturaji/assets/Chess_prg45.svg"); // Replace with your image file path
 
     // Set the pixmap to the QLabel
     imageLabel->setPixmap(pixmap);
@@ -25,10 +24,7 @@ PieceView::PieceView(QWidget *parent)
     // Add QLabel to the layout
     layout->addWidget(imageLabel);
 
-    // Set the layout on the main window
     this->setLayout(layout);
-
-    // Set window properties
     this->setWindowTitle("Image Display Example");
-    this->resize(100, 100); // Optional window size adjustment
+    this->resize(100, 100);
 }

1b20271ee4e5f295f068c69b50778471d11fa485
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-11-08 15:21:53 +0100

    Added piece to main window

--- Chaturaji/src/view/MainWindow.cpp
+++ Chaturaji/src/view/MainWindow.cpp
@@ -8,6 +8,7 @@
 #include <QGraphicsScene>
 #include <QGraphicsRectItem>
 #include <QPushButton>
+#include <QVBoxLayout>
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 #include "BoardView.h"
@@ -18,13 +19,27 @@ MainWindow::MainWindow(QWidget *parent) :
         QWidget(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 
-    auto b = new BoardView(this);
+//    auto b = new BoardView(this);
 
 //    auto m_scene = new QGraphicsScene(this);
 //    auto m_view = new QGraphicsView(m_scene, this);
 //    auto rect = new QGraphicsRectItem(10,10,100,100);
 //    rect->setBrush(QBrush(Qt::darkRed));
 //    m_scene->addItem(rect);
+
+    // Create the main layout for MainWindow
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+
+    // Create and add BoardView to the layout
+    auto boardView = new BoardView(this);
+    mainLayout->addWidget(boardView);
+
+    // Create and add PieceView to the layout
+    auto pieceView = new PieceView(this);
+    mainLayout->addWidget(pieceView);
+
+    // Set the layout for MainWindow
+    this->setLayout(mainLayout);
 
 }
 

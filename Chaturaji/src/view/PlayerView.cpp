b91b93134635b6648f9233a2539d99e88a66bc93
RobinLambregts <robin.lambregts@student.uhasselt.be>
2024-11-16 17:02:14 +0100

    styling added

--- Chaturaji/src/view/PlayerView.cpp
+++ Chaturaji/src/view/PlayerView.cpp
@@ -18,6 +18,8 @@ PlayerView::PlayerView(QWidget *parent, Player *player, Game* game)
 void PlayerView::paintEvent(QPaintEvent *event) {
     QWidget::paintEvent(event);
     QPainter painter(this);
+    QPainterPath path;
+    QPen strokePen;
 
     if (*player == game->getCurrentPlayer()) { // Use the Player method to check turn
         setMinimumSize(250, 250); // Larger size for the current player
@@ -25,16 +27,16 @@ void PlayerView::paintEvent(QPaintEvent *event) {
         setMinimumSize(250, 70); // Default size for others
     }
 
-//    QRect boundingBox((width() - width()) / 2, (height() - height()) / 2, width(), height());
+    //    QRect boundingBox((width() - width()) / 2, (height() - height()) / 2, width(), height());
     QRect boundingBox(0, 0, width(), height());
     colour = player->getQColour();
-    QPainterPath path;
     path.addRoundedRect(boundingBox, 15, 15);  // Rounded corners with radius 15
-    painter.fillPath(path, colour);
+    painter.fillPath(path, QColorConstants::Svg::grey);
 
     // Tekst setup
     QFont font = painter.font();
     font.setPointSize(14);
+    font.setBold(true);
     painter.setFont(font);
     painter.setPen(Qt::white);
 
@@ -43,6 +45,10 @@ void PlayerView::paintEvent(QPaintEvent *event) {
     painter.drawText(padding, 30, "Name: " + player->getName());
     painter.drawText(padding, 60, "Score: " + QString::number(player->getScore()));
 
-    painter.setPen(Qt::black);
+    strokePen.setColor(colour);
+    strokePen.setWidth(10);
+    strokePen.setJoinStyle(Qt::MiterJoin);
+    painter.setPen(strokePen);
+
     painter.drawPath(path);
 }

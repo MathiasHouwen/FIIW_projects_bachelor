0327f88c2331fc3987a068473e69fcc2511c8a12
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-11-18 19:13:42 +0100

    CLI module

---
+++ PRACTICUM_2_NETFLIX/src/modules/CLI.cpp
@@ -0,0 +1,67 @@
+//
+// Created by houwe on 18/11/2024.
+//
+
+#include <iostream>
+#include <sstream>
+#include "CLI.h"
+
+CLI::Command CLI::getInput(const string &prompt) {
+    Command cmd;
+    string inputLine;
+    cout << prompt;
+    getline(cin, inputLine);
+
+    istringstream iss(inputLine);
+    string word;
+
+    // Het eerste woord in de string stream wordt het command
+    if (iss >> word) {
+        cmd.commandName = word;
+    } else {
+        throw runtime_error("Geen command");
+    }
+
+    parseToken(iss, cmd);
+
+    return cmd;
+}
+
+void CLI::parseToken(istringstream& iss, CLI::Command& cmd){
+    string word;
+    if (!(iss >> word)) {
+        return;
+    }
+
+    if (word.rfind("--", 0) == 0) {
+        cmd.flags.insert(word);
+    } else if (word.rfind('-', 0) == 0){
+        string param = word;
+
+        if (!(iss >> word)) {
+            throw runtime_error("Parameter waarden mist voor " + param);
+        }
+
+        cmd.params[param] = word;
+    } else {
+        throw runtime_error("Invalid argument: " + word);
+    }
+
+    parseToken(iss, cmd);
+}
+
+bool CLI::validate(CLI::Command cmd, set<string> allowedParams, set<string> allowedFlags) {
+    for(auto [param, value] : cmd.params){
+        if (allowedParams.find(param) == allowedParams.end()) {
+            return false;
+        }
+    }
+
+    for ( auto flag : cmd.flags) {
+        if (allowedFlags.find(flag) == allowedFlags.end()) {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ PRACTICUM_2_NETFLIX/src/modules/CLI.h
@@ -0,0 +1,31 @@
+//
+// Created by houwe on 18/11/2024.
+//
+
+#ifndef PRACTICUM_2_NETFLIX_CLI_H
+#define PRACTICUM_2_NETFLIX_CLI_H
+
+#include <string>
+#include <set>
+#include <map>
+
+using namespace std;
+
+class CLI {
+public:
+    struct Command{
+        string commandName;
+        map<string, string> params;
+        set<string> flags;
+    };
+
+    Command getInput(const string &prompt);
+    bool validate(Command cmd, set<string> allowedParams, set<string> allowedFlags);
+
+private:
+    void parseToken(istringstream& iss, CLI::Command& cmd);
+
+};
+
+
+#endif //PRACTICUM_2_NETFLIX_CLI_H

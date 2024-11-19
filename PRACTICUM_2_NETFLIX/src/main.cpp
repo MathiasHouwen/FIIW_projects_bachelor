a5012dd853d1367ba783945e092b2b38a38735c3
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2024-11-19 09:45:35 +0100

    added csv library

--- PRACTICUM_2_NETFLIX/src/main.cpp
+++ PRACTICUM_2_NETFLIX/src/main.cpp
@@ -2,10 +2,14 @@
 // Created by robin on 18/11/2024.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
 
+
+
+#include <internal/csv_reader.hpp>
+
+using namespace csv;
 int main() {
-    printf("Hello World\n");
+    CSVReader reader("../data/netflixdata.csv");
+
     return 0;
 }

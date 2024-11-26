8b9675865d1bdb56cd998d646875cf6ecd71a2f8
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2024-11-26 13:45:02 +0100

    Oeps, sorry guys compiler error

--- PRACTICUM_2_NETFLIX/src/main.cpp
+++ PRACTICUM_2_NETFLIX/src/main.cpp
@@ -17,19 +17,20 @@ using namespace std;
 int main() {
     //CSVReader reader("../data/netflixdata.csv");
     Netflix netflix;
-    auto* action = new string("action");
-    auto* romance = new string("romance");
-    MovieOrShow movie1 = {"Armageddon", action, 5.5};
-    MovieOrShow movie2 = {"Cars", romance, 9.5};
-    MovieOrShow movie3 = {"Arme man", romance, 5.5};
-    MovieOrShow movie4 = {"Arme man", action, 5.5};
-    Trie *trie = new Trie();
-    trie->insertMOS(&movie1);
-    trie->insertMOS(&movie2);
-    trie->insertMOS(&movie3);
-    trie->insertMOS(&movie4);
-    for(auto mos:trie->search("Arm", action)) {
-        cout << " - " << mos.toString() << endl;
-    }
+
+//    auto* action = new string("action");
+//    auto* romance = new string("romance");
+//    MovieOrShow movie1 = {"Armageddon", action, 5.5};
+//    MovieOrShow movie2 = {"Cars", romance, 9.5};
+//    MovieOrShow movie3 = {"Arme man", romance, 5.5};
+//    MovieOrShow movie4 = {"Arme man", action, 5.5};
+//    Trie *trie = new Trie();
+//    trie->insertMOS(&movie1);
+//    trie->insertMOS(&movie2);
+//    trie->insertMOS(&movie3);
+//    trie->insertMOS(&movie4);
+//    for(auto mos:trie->search("Arm", action)) {
+//        cout << " - " << mos.toString() << endl;
+//    }
     return 0;
 }

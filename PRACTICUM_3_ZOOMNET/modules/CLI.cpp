24a627ab53d4ca55357a93cfe47b911fe022692c
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-12-05 10:31:54 +0100

    CLI boilerplate code

---
+++ PRACTICUM_3_ZOOMNET/modules/CLI.cpp
@@ -0,0 +1,55 @@
+//
+// Created by houwe on 27/11/2024.
+//
+
+#include <iostream>
+#include "CLI.h"
+
+using namespace std;
+
+bool CLI::getInput() {
+    try {
+        CommandParser::Command cmd = parser.getInput(prompt);
+
+        if (commands.find(cmd.commandName) == commands.end()) {
+            cout << "Unknown command: " << cmd.commandName << "\n";
+            return false;
+        }
+
+        if (!parser.validate(cmd, allowedParams, allowedFlags)) {
+            cout << "Command validation gefaald.\n";
+            return false;
+        }
+
+        logic(cmd);
+
+    } catch (const std::exception& e){
+        cout << "Error: " << e.what() << "\n";
+        return false;
+    }
+
+    return true;
+}
+
+void CLI::logic(CommandParser::Command cmd) {
+    if (commands.find(cmd.commandName) != commands.end()) {
+        commands[cmd.commandName](cmd.params, cmd.flags);
+    }
+}
+
+void CLI::registerCommand(const string &name, function<void(unordered_map<string, string>, unordered_set<string>)> cmd,
+                          const unordered_set<string> &params, const unordered_set<string> &flags) {
+
+    commands[name] = [cmd, params, flags](unordered_map<string, string> funcParams,
+                                          unordered_set<string> funcFlags){
+        for (auto &key : params){
+            funcParams.emplace(key, "");
+        }
+        funcFlags.insert(flags.begin(), flags.end());
+        cmd(std::move(funcParams), std::move(funcFlags));
+    };
+
+}
+
+CLI::CLI(const unordered_set<string> &allowedParams, const unordered_set<string> &allowedFlags) : allowedParams(
+        allowedParams), allowedFlags(allowedFlags) {}
---
+++ PRACTICUM_3_ZOOMNET/utils/ZoomNetCLI.cpp
@@ -0,0 +1,31 @@
+//
+// Created by houwe on 5/12/2024.
+//
+
+#include "ZoomNetCLI.h"
+#include "../modules/CLI.h"
+
+
+ZoomNetCLI::ZoomNetCLI() {
+    CLI cli({}, {});
+    cli.registerCommand("design", &ZoomNetCLI::design, {}, {});
+    cli.registerCommand("adapt", &ZoomNetCLI::adapt, {}, {});
+    cli.registerCommand("optimize", &ZoomNetCLI::optimize, {}, {});
+    cli.registerCommand("maintain", &ZoomNetCLI::maintain, {}, {});
+}
+
+void ZoomNetCLI::design(unordered_map<string, string> params, unordered_set<string> flags) {
+
+}
+
+void ZoomNetCLI::adapt(unordered_map<string, string> params, unordered_set<string> flags) {
+
+}
+
+void ZoomNetCLI::optimize(unordered_map<string, string> params, unordered_set<string> flags) {
+
+}
+
+void ZoomNetCLI::maintain(unordered_map<string, string> params, unordered_set<string> flags) {
+
+}

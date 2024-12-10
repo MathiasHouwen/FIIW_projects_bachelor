62c1822ff751ac8d9d6595535f5c127845787a75
RobinLambregts <robin.lambregts@student.uhasselt.be>
2024-12-10 10:03:28 +0100

    MST complete (very JIPIE moment)

--- PRACTICUM_3_ZOOMNET/src/Graph.cpp
+++ PRACTICUM_3_ZOOMNET/src/Graph.cpp
@@ -8,7 +8,7 @@ void Graph::addNode(CityNode *node) {
     nodes[node->city] = node;
 }
 
-void Graph::removeNode(CityNode *node) {
+void Graph::removeNode(const CityNode *node) {
     if (nodes[node->city]) {
         nodes.erase(node->city);
     }
@@ -18,34 +18,31 @@ void Graph::removeNode(CityNode *node) {
 }
 
 void Graph::setConnection(CityNode *source, CityNode *destination, int connection) {
-    if (source->connections.contains({source, destination, connection})) {
+    auto* newConnection = new Connection({source, destination, connection});
+    if (source->connections.contains(newConnection)) {
         cout << "Connection already exists" << endl;
     }
     else {
-        auto* newConnection = new Connection({source, destination, connection});
         allConnections.insert(newConnection);
-        source->connections.insert(*newConnection);
-        destination->connections.insert(*newConnection);
+        source->connections.insert(newConnection);
+        destination->connections.insert(newConnection);
     }
 }
 
-std::set<Connection> Graph::getConnections(string city) {
+std::set<Connection*> Graph::getConnections(const string& city) {
     return nodes[city]->connections;
 }
 
-CityNode* Graph::getNode(string city) {
+CityNode* Graph::getNode(const string& city) {
     return nodes[city];
 }
 
 void Graph::getShortestPath() {
-    visitCity(0);
-}
-
-void Graph::visitCity(int distanceTraveled) {
+    int distanceTraveled = 0;
     for (Connection* connection : allConnections) {
         unordered_set<string> visitedNodes;
         visitedNodes.insert(connection->start->city);
-        if (!checkCycle(connection->destination, visitedNodes)) {
+        if (!checkCycle(connection->destination, connection->start, visitedNodes)) {
             connection->realityCheck = true;
             cout << "Connection: " << connection->start->city << "->" << connection->destination->city << endl;
             distanceTraveled += connection->weight;
@@ -54,7 +51,7 @@ void Graph::visitCity(int distanceTraveled) {
     cout << "Distance: " << distanceTraveled << endl;
 }
 
-void Graph::printroute(unordered_set<string> &visitedNodes, int distanceTraveled) {
+void Graph::printroute(const unordered_set<string> &visitedNodes, const int distanceTraveled) {
     cout << "Route: " << endl;
     for(const string& city : visitedNodes) {
         cout << city << " ";
@@ -62,15 +59,15 @@ void Graph::printroute(unordered_set<string> &visitedNodes, int distanceTraveled
     cout << endl << "Distance: " << distanceTraveled << endl;
 }
 
-bool Graph::checkCycle(CityNode* currentNode, unordered_set<string> &visitedNodes) {
+bool Graph::checkCycle(const CityNode* currentNode, const CityNode* prevNode, unordered_set<string> &visitedNodes) {
     if (visitedNodes.contains(currentNode->city)){return true;}
     visitedNodes.insert(currentNode->city);
-    for (const Connection& connection : currentNode->connections) {
-        if (!connection.realityCheck) {
+    for (const Connection* connection : currentNode->connections) {
+        if (!connection->realityCheck) {
             continue;
         }
-        CityNode* nextCity = connection.destination == currentNode ? connection.start : connection.destination;
-        if (checkCycle(nextCity, visitedNodes)) {return true;}
+        CityNode* nextCity = connection->destination == currentNode ? connection->start : connection->destination;
+        if (nextCity != prevNode && checkCycle(nextCity, currentNode, visitedNodes)) {return true;}
     }
     return false;
 }
--- PRACTICUM_3_ZOOMNET/src/Graph.h
+++ PRACTICUM_3_ZOOMNET/src/Graph.h
@@ -21,7 +21,7 @@ using namespace std;
 
 struct CityNode{
   std::string city;
-  set<Connection> connections;
+  set<Connection*> connections;
 };
 
 struct Connection {
@@ -46,18 +46,17 @@ public:
   Graph() = default;
   ~Graph() = default;
   void addNode(CityNode* node);
-  void removeNode(CityNode* node);
-  CityNode* getNode(string city);
+  void removeNode(const CityNode* node);
+  CityNode* getNode(const string& city);
   void getShortestPath();
-  std::set<Connection> getConnections(string city);
+  std::set<Connection*> getConnections(const string& city);
   void setConnection(CityNode* source, CityNode* destination, int connection);
 
 private:
   unordered_map<string, CityNode*> nodes;
   std::set<Connection*, ConnectionPointerComparator> allConnections;
-  bool checkCycle(CityNode* start, unordered_set<string> &visitedNodes);
-  void visitCity(int distanceTraveled);
-  static void printroute(unordered_set<string> &visitedNodes, int distanceTraveled);
+  bool checkCycle(const CityNode* start, const CityNode* prevNode, unordered_set<string> &visitedNodes);
+  static void printroute(const unordered_set<string> &visitedNodes, int distanceTraveled);
 };
 
 

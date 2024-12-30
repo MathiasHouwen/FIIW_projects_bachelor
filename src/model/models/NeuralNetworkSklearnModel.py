e499e11c4168d5397b6715d052af555ecc373e32
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-12-30 21:02:45 +0100

    Using GridSearchCV for hyperparameter tuning

--- src/model/models/NeuralNetworkSklearnModel.py
+++ src/model/models/NeuralNetworkSklearnModel.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPRegressor
 from src.model.ModelHandler import MLModel
 
@@ -7,11 +8,27 @@ class NeuralNetworkSklearnModel(MLModel):
     def __init__(self):
         super().__init__()
 
-        # self.scikit_model = LinearRegression(fit_intercept=True)
-        self.scikit_model = MLPRegressor(hidden_layer_sizes=(10, 10), max_iter=1000, random_state=42)
+        self.param_grid = {
+            'hidden_layer_sizes': [(10, 10), (32, 32), (64, 64, 32)],
+            'activation': ['relu', 'tanh'],
+            'alpha': [0.0001, 0.001, 0.01],  # Regularization term
+            'learning_rate_init': [0.001, 0.01, 0.1],  # Initial learning rate
+            'max_iter': [500, 1000]
+        }
+
+        self.scikit_model = MLPRegressor(random_state=42, verbose=True)
 
     def train(self, x_features: np.array, y_feature: np.array):
-        self.scikit_model.fit(x_features, y_feature)
+        param_grid = self.param_grid
+        mlp = self.scikit_model
+
+        # Using GridSearchCV for hyperparameter tuning
+        grid_search = GridSearchCV(mlp, param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
+        grid_search.fit(x_features, y_feature)
+
+        # Find best model
+        best_model = grid_search.best_estimator_
+        self.scikit_model = best_model
 
     def predict(self, x_features: np.array) -> np.array:
         return self.scikit_model.predict(x_features)

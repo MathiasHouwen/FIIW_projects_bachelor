4e72a479c1349dab462b42731191993c2976847d
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2025-03-06 14:25:06 +0100

    dirty code to find defects when there is no noice or distortion

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import cv2
 from src.utils.preprocesing import *
 import matplotlib.pyplot as plt
 import numpy as np
+from skimage.metrics import structural_similarity as ssim
 
-
-def main():
+def test_4_removenoise():
     # img = load_as_gray('./../images/test_1/input/01_missing_hole_01.jpg')
     # cv2.imshow('Grayscale', img)
 
@@ -44,11 +44,54 @@ def main():
     plt.tight_layout()
     plt.show()
 
+
+def main():
+    #test_4_removenoise()
+
+    # Load images
+    template_path = "./../images/template_images/01.JPG"  # Path to the correct PCB image
+    test_path = "./../images/test_1/input/01_missing_hole_01.jpg"
+
+    template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
+    test = cv2.imread(test_path, cv2.IMREAD_GRAYSCALE)
+
+    # Compute Structural Similarity Index (SSIM) and get the difference image
+    score, diff = ssim(template, test, full=True)
+    diff = (diff * 255).astype("uint8")
+
+    # Threshold the difference image
+    _, thresh = cv2.threshold(diff, 50, 255, cv2.THRESH_BINARY_INV)
+
+    thresh_bgr = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+
+    # Find contours in the thresholded image
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Set minimum area to filter out small contours (adjust based on your needs)
+    min_area = 1
+    test_w_box = cv2.imread(test_path)
+    # Draw red rectangles around significant contours
+    for contour in contours:
+        if cv2.contourArea(contour) > min_area:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(test_w_box, (x, y), (x + w, y + h), (0, 0, 255), 2)  # Red in BGR
+
+    # Convert BGR to RGB for correct display in Matplotlib
+    imgbox = cv2.cvtColor(test_w_box, cv2.COLOR_BGR2RGB)
+
+    # Display the result
+    plt.imshow(imgbox)
+    plt.title("Thresholded Defect Areas with Red Boxes")
+    plt.axis("off")
+    plt.show()
+
+    # img = load_as_gray('./../images/test_1/input/01_missing_hole_01.jpg')
+    # cv2.imshow('Grayscale', img)
+
     # fft = fourier_transform(img)
     #
     # # cv2.imshow('', img)
     # cv2.imshow('', fft)
-
 
     #fshift = fourier_transform(img)
     #mask = create_filter(img)
@@ -56,10 +99,8 @@ def main():
     #image_filtered = inverse_fourier_transform(masked)
     #cv2.imshow('lowpass', image_filtered)
 
-
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':

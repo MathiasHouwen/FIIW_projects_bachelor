ec50e77aedd2afbd10ad2718a0c03cb9fb2fd585
Ebbe Wertz <146071304+EbbeWertzStudentUH@users.noreply.github.com>
2025-03-10 14:47:47 +0100

    padding toegevoegd aan postprocesing.py

--- src/utils/postprocesing.py
+++ src/utils/postprocesing.py
@@ -1,15 +1,12 @@
 import cv2
 
-# TODO: ADD PADDING FOR SMALL BOXES
-def draw_bounding_boxes(test_path, contours, min_area = 50):
+def draw_bounding_boxes(test_path, contours, min_area = 50, pad=5):
     # Set minimum area to filter out small contours (adjust based on your needs)
     test_w_box = cv2.imread(test_path)
     # Draw red rectangles around significant contours
     for contour in contours:
         if cv2.contourArea(contour) > min_area:
             x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(test_w_box, (x, y), (x + w, y + h), (0, 0, 255), 2)  # Red in BGR
+            cv2.rectangle(test_w_box, (x-pad, y-pad), (x + w + pad, y + h + pad), (255, 0, 0), 3)  # Red in BGR
 
-    # Convert BGR to RGB for correct display in Matplotlib
-    imgbox = cv2.cvtColor(test_w_box, cv2.COLOR_BGR2RGB)
-    return imgbox
+    return test_w_box

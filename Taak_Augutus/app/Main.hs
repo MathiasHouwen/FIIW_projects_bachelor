b4142a44b60f8ee1054414cae1d2135c06ea8ab8
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2025-08-07 15:49:13 +0200

    Very basic swaping - Without algo

--- Taak_Augutus/app/Main.hs
+++ Taak_Augutus/app/Main.hs
@@ -4,15 +4,19 @@ import qualified Data.Set as Set
 import MaxBipartiteMatching (matching)
 
 edges :: Set.Set (Char, Char)
-edges = Set.fromList 
-    [ ('k','k'), ('k','c')
-    , ('c','c'), ('c','k')
-    , ('l','l'), ('l','k')
-    , ('q','q'), ('q','k'), ('q','c')
-    ]
+edges = Set.fromList
+  [ ('k', 'c'), ('k', 'd'), ('k', 'e')
+  , ('c', 'a'), ('c', 'd'), ('c', 'f')
+  , ('l', 'b'), ('l', 'e'), ('l', 'f')
+  , ('q', 'a'), ('q', 'b'), ('q', 'c'), ('q', 'e')
+  , ('m', 'd'), ('m', 'f'), ('m', 'g')
+  , ('n', 'a'), ('n', 'g'), ('n', 'h')
+  ]
 
 main :: IO ()
 main = do
     let filteredEdges = Set.filter (\(x,y) -> x /= y) edges
     let result = matching filteredEdges
     print result
+    let l = length result
+    print l
